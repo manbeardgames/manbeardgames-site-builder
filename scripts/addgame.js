5ae94426b6efb931f5c962adb981cbca67992b5d
whitleych17 <whitleych17@ecu.edu>
2018-11-21 14:53:26 -0500

    Added addgame script (not fully finished)

--- scripts/addgame.js
+++ scripts/addgame.js
@@ -1,336 +1,146 @@
 const fse = require('fs-extra');
-const path = require('path');
-
-let game = {
-    "name": "",
-    "shortDescription": "",
-    "description": [],
-    "trailer": "",
-    "platforms": [],
-    "os": {
-        "windows": false,
-        "osx": false,
-        "switch": false,
-        "xone": false,
-        "ps4": false,
-        "android": false,
-        "ios": false
-    }
-};
-
-let questions = [
-    'name',
-    'short',
-    'full',
-    'trailer',
-    'platforms',
-    'os'
-]
-
-let idx = 0;
-
-AskName();
-AskShort();
-AskFull();
-AskTrailer();
-AskPlatforms();
-AskOS();
-
-// while (idx < questions.length) {
-//     var question = questions[idx];
-//     switch (question) {
-//         case 'name':
-//             idx = idx + AskName();
-//             break;
-//         case 'short':
-//             idx = idx + AskShort();
-//             break;
-//         case 'full':
-//             idx = idx + AskFull();
-//             break;
-//         case 'trailer':
-//             idx = idx + AskTrailer();
-//             break;
-//         case 'platforms':
-//             idx = idx + AskPlatforms();
-//             break;
-//         case 'os':
-//             idx = idx + AskOS();
-//             break;
-//     }
-// }
-
-
-//  Convert the game object to JSON
-let json = JSON.stringify(game);
-
-//  Create the file name
-let fileName = path.join('./source', 'data', 'games', `${game.name}.json`);
-
-//  Write the json to disk
-fse.writeFileSync(fileName, json);
-
-
-
-
-function prompt(question, callback) {
-    var stdin = process.stdin;
-    var stdout = process.stdout;
-
-    stdin.resume();
-    stdout.write(question);
-
-    stdin.once('data', function (data) {
-        callback(data.toString().trim());
-    });
-}
+const read = require('readline-sync');
 
 
 
 
 
+let game = {};
 
 
 
+game.name = AskName();
+game.shortDescription = AskShort();
+game.descriptio = AskFull();
+game.trailer = AskTrailer();
+game.platforms = AskPlatforms();
+game.os = AskOS();
+
+//  Create JSOn from game
+let json = JSON.stringify(game, null, 4);
+
+//  Write json to disk
+fse.writeFileSync('./.tmp/promptTest.json', json);
 
 
 
 
 function AskName() {
+    //  Ask the question
+    let result = read.question('game name: (My Game) ');
 
-    let val = 0;
-    prompt("What is the name of the game?", function (input) {
-        let name = SanatizeInput(input);
+    //  Parse the result
+    if (!result || result === '') {
+        result = 'My Game';
+    }
 
-        if (name === '' || name === null) {
-            name = "Example Name";
-        }
-
-        game.name = name;
-        val = 1;
-    });
-
-    return val;
-
-    // process.stdout.write('What is the name of the game? ' + '\n');
-    // let name = process.stdin.read();
-
-    // return 1;
-
+    //  Return the result
+    return result;
 }
 
 function AskShort() {
+    //  Ask the question
+    let result = read.question('short description: ');
 
-    let val = 0;
-    prompt("Give a short description of the game (1-2 sentences", function (input) {
-        let short = SanatizeInput(input);
+    //  Parse the result
+    if (!result) {
+        result = '';
+    }
 
-        if (short === null) {
-            short = '';
-        }
-
-        game.shortDescription = short;
-        val = 1;
-    });
-
-    return val;
-
-    // process.stdout.write('Give a short description of the game (1-2 sentences' + '\n');
-
-    // let short = process.stdin.read();
-
-    // return 1;
+    //  Return the result
+    return result;
 }
 
 function AskFull() {
-    let val = 0;
+    //  Setup the array to hold the user answers
+    let result = [];
+    let count = 1;
 
-    console.log("Give a full description of the game");
-    console.log("Enter one paragraph at a time.  If finished, leave balnk and hit enter");
+    //  Setup loop
+    let resolved = false;
 
-    prompt('', function (input) {
-        let full = SanatizeInput(full);
-        if (full === '') {
-            val = 1;
+    while (!resolved) {
+        //  Ask the question
+        let answer = read.question('Full Description: (blank when finished) ');
+
+        if (!answer || answer === '') {
+            resolved = true;
         } else {
-            game.description.push(full);
-            val = 0;
+            count++;
+            result.push(answer);
         }
-    });
+    }
 
-    return val;
-
-    // process.stdout.write('Give a full description of the game' + '\n');
-    // process.stdout.write('Enter one paragraph at a time.  If finished, leave blank and hit enter' + '\n');
-
-    // let full = process.stdin.read();
-    // full = SanatizeInput(full);
-
-    // if (full === '') {
-    //     return 1;
-    // } else {
-    //     game.description.push(full);
-    //     return 0;
-    // }
+    //  Return the result
+    return result;
 }
 
 function AskTrailer() {
-    let val = 0;
+    //  Ask question
+    let result = read.question('trailer: (youtube only) ');
 
-    prompt('Give the link to the trailer on youtube', function (input) {
-        let trailer = input;
+    //  Parse the result
+    if (!result) {
+        result = '';
+    }
 
-        if (trailer === null) {
-            trailer = '';
-        }
-        game.trailer = trailer;
-        val = 1;
-    });
-
-    return val;
-
+    //  Return the result
+    return result;
 }
 
 function AskPlatforms() {
+    //  Setup the array to hold the user answer
+    let result = [];
 
-    console.log('Enter a platform thsi has been released on (steam, itch, and ludum are the only ones acceptable');
-    console.log('If finished, entering platforms, leave blank and press enter');
+    //  Setup loop
+    let count = 1;
+    let resolved = false;
 
-    let val = 0;
-    prompt('', function (input) {
-        let name = SanatizeInput(input);
+    while (!resolved) {
+        //  Ask which platform
+        let platform = read.question('platform: (name, url) ');
 
-        if (name === 'steam' || name == 'itch' || name === 'ludum') {
-            console.log('Give the link to the game on that platform');
-            prompt('', function (inputtwo) {
-                let link = inputtwo;
-                if (link === null) {
-                    link = '';
-                }
-
-                platforms.push({ name: name, link: link });
-                val = 0;
-            });
+        if (!platform || platform === '') {
+            resolved = true;
         } else {
-            val = 1;
+            let [name, url] = platform.split(','),
+                answer = { name: name, url: url };
+
+            result.push(answer);
         }
-    });
+    }
 
-    return val;
-
-    // process.stdout.write('Enter a platform this has been released on (steam, itch, ludum are the only acceptable ones atm' + '\n');
-    // process.stdout.write('If finished entering platforms, leave blank and press enter');
-
-    // let name = process.stdin.read();
-    // name = SanatizeInput(name);
-
-    // if (name === 'steam' || name === 'itch' || name === 'ludum') {
-    //     process.stdout.write('Give the link to the game on that platform' + '\n');
-    //     let link = process.stdin.read();
-    //     if (link === null) {
-    //         link = '';
-    //     }
-
-    //     platforms.push({ name: name, link: link });
-    //     return 0;
-    // } else {
-    //     return 1;
-    // }
+    //  Return the result
+    return result;
 }
 
 function AskOS() {
 
-    let win = false;
-    let osx = false;
-    let nswitch = false;
-    let xone = false;
-    let ps4 = false;
-    let android = false;
-    let ios = false;
+    //  Ask the questions
+    let win = read.question('window: (y/n) ');
+    let osx = read.question('macos: (y/n) ');
+    let linux = read.question('linux: (y/n) ');
+    let nswitch = read.question('nintendo switch: (y/n) ');
+    let xone = read.question('xbox one: (y/n) ');
+    let ps4 = read.question('playstation 4: (y/n) ');
+    let anroid = read.question('android: (y/n) ');
+    let ios = read.question('iphone: (y/n) ');
 
-    prompt('Availalbe on Windows? (y/n)', function(input) {
-        let answer = input;
-        win = answer.toLowerCase() === 'y' ? true : false;
-    });
+    //  Parse the results into an object
+    let os = {
+        win: win === 'y' ? true : false,
+        osx: osx === 'y' ? true : false,
+        linux: linux === 'y' ? true : false,
+        nswitch: nswitch === 'y' ? true : false,
+        xone: xone === 'y' ? true : false,
+        ps4: ps4 === 'y' ? true : false,
+        anroid: anroid === 'y' ? true : false,
+        ios: ios === 'y' ? true : false
+    }
 
-    prompt('Availalbe on Mac OSX? (y/n)', function(input) {
-        let answer = input;
-        osx = answer.toLowerCase() === 'y' ? true : false;
-    });
-    
-    
-    prompt('Availalbe on Nintendo Switch? (y/n)', function(input) {
-        let answer = input;
-        nswitch = answer.toLowerCase() === 'y' ? true : false;
-    });
-    
-    
-    prompt('Availalbe on Xbox One? (y/n)', function(input) {
-        let answer = input;
-        xone = answer.toLowerCase() === 'y' ? true : false;
-    });
-    
-    
-    prompt('Availalbe on Playstation 4? (y/n)', function(input) {
-        let answer = input;
-        ps4 = answer.toLowerCase() === 'y' ? true : false;
-    });
-    
-    
-    prompt('Availalbe on Android? (y/n)', function(input) {
-        let answer = input;
-        android = answer.toLowerCase() === 'y' ? true : false;
-    });
-    
-    
-    prompt('Availalbe on Apple iPhones? (y/n)', function(input) {
-        let answer = input;
-        ios = answer.toLowerCase() === 'y' ? true : false;
-    });
+    //  Return the result
+    return os;
 
-    game.os.win = win;
-    game.os.osx = osx;
-    game.os.switch = nswitch;
-    game.os.xone = xone;
-    game.os.ps4 = ps4;
-    game.os.android = android;
-    game.os.ios = ios;
-
-    return 1;
-    
-    
-
-
-    // process.stdout.write('Avaialble on Windows? (y/n): ');
-    // let win = process.stdin.read();
-
-    // process.stdout.write('\n' + 'osx? (y/n) ');
-    // let osx = process.stdin.read();
-
-    // process.stdout.write('\n' + 'switch? (y/n) ');
-    // let nswitch = process.stdin.read();
-
-    // process.stdout.write('\n' + 'xone? (y/n) ');
-    // let xone = process.stdin.read();
-
-    // process.stdout.write('\n' + 'ps4? (y/n) ');
-    // let ps4 = process.stdin.read();
-
-    // process.stdout.write('\n' + 'android? (y/n) ');
-    // let android = process.stdin.read();
-
-    // process.stdout.write('\n' + 'ios? (y/n) ');
-    // let ios = process.stdin.read();
-
-    // game.os.win = win.toLowerCase() === 'y' ? true : false;
-    // game.os.osx = osx.toLowerCase() === 'y' ? true : false;
-    // game.os.switch = nswitch.toLowerCase() === 'y' ? true : false;
-    // game.os.xone = xone.toLowerCase() === 'y' ? true : false;
-    // game.os.ps4 = ps4.toLowerCase() === 'y' ? true : false;
-    // game.os.android = android.toLowerCase() === 'y' ? true : false;
-    // game.os.ios = ios.toLowerCase() === 'y' ? true : false;
-
-    // return 1;
 }
 
 function SanatizeInput(str) {
